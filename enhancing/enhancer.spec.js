9cf33090399e61aa382e22a35458136e7619e747
Samantha Goodman <antashma@gmail.com>
2020-12-08 12:03:56 -0500

    add first test and add jest

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,18 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('repair item', () => {
+    it('returns item with 100 durability', () => {
+        const testItem = {
+            name: 'test sword',
+            enhancement: 0,
+            durability:0,
+        }
+        const expectedItem = {
+            name: 'test sword',
+            enhancement: 0,
+            durability:100,
+        }
+        expect(enhancer(add(testItem))).toBe(expectedItem);
+    })
+})
