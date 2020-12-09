577cc1086cb917dc07387cbc1aa3c2ac0e0c5169
Samantha Goodman <antashma@gmail.com>
2020-12-08 20:08:21 -0500

    mvp complete

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -8,6 +8,7 @@ module.exports = {
 function success(item) {
   if(item.enhancement < 20) {
     item.enhancement+= 1;
+    item = checkEnhancement(item);
     return {...item}
   } else {
     return { ...item };
@@ -15,12 +16,24 @@ function success(item) {
 }
 
 function fail(item) {
-  if (item.enhancement < 15) {
-    item.durability -= 5;
-    return {...item};
-  } else {
-    return { ...item };
+  switch(true) {
+    case item.enhancement < 15:
+      item.durability -= 5;
+      item = checkDurability(item);
+      return {...item};
+    case item.enhancement >= 15:
+      if (item.enhancement > 16) {
+        item = checkEnhancement(item);
+        --item.enhancement;
+      };
+      item.durability -= 10;
+      item = checkDurability(item);
+      console.log(item);
+      return {...item};
+    default:
+      return {...item};
   }
+
 }
   
 
@@ -31,4 +44,34 @@ function repair(item) {
 
 function get(item) {
   return { ...item };
+}
+
+function checkEnhancement(item) {
+  switch (true) {
+    case !item.enhancement:
+      console.log(`This ${item.name} cannot be enhanced.`);
+      return `This ${item.name} cannot be enhanced.`
+    case item.enhancement > 20:
+      return {...item, enhancement: 20}
+    case item.enhancement < 0:
+      return {...item, enhancement: 0}
+    default:
+      return item;
+  } 
+}
+
+function checkDurability(item) {
+  switch (true) {
+    case !item.durability:
+      console.log(item);
+      console.log(`This ${item.name} cannot be repaired.`)
+      return `This ${item.name} cannot be repaired.`
+    case item.durability > 100:
+      return {...item, durability: 100}
+    case item.durability < 0:
+      return {...item, durability: 0}
+    default:
+      return item;
+  } 
+
 }
--- enhancing/enhancer.test.js
+++ enhancing/enhancer.test.js
@@ -1,79 +1,83 @@
 const enhancer = require('./enhancer.js');
 // test away!
 
-describe('enhance item - repair', () => {
-    const testItem = {
-        name: 'test sword',
-        enhancement: 0,
-        durability:0,
-    }
+const testItem1 = {
+    name: 'old shield',
+    enhancement: 13,
+    durability: 0,
+}
+const testItem2 = {
+    name: 'wooden sword',
+    enhancement: 18,
+    durability: 20,
+}
+const testItem3 = {
+    name: 'lucky amulet',
+    enhancement: 20,
+    durability: 0,
+}
 
+const brokenItem = {
+    name: 'stick',
+    durability: 13,
+}
+
+describe('item to be enhanced', () => {
+    it('has name, durability, and enhancement properties', () => {
+        expect(testItem1).toHaveProperty('enhancement');
+        expect(testItem1).toHaveProperty('durability');
+        expect(testItem1).toHaveProperty('name');
+    })
+})
+
+describe('enhance item - repair', () => {
     it('returns item with 100 durability', () => {
-        const expectedItem = {
-            name: 'test sword',
-            enhancement: 0,
-            durability:100,
-        }
-        expect(enhancer.repair(testItem)).toStrictEqual(expectedItem);
+        expect(enhancer.repair({...testItem1}).durability).toStrictEqual(100);
+        expect(enhancer.repair({...testItem2}).durability).toStrictEqual(100);
+        expect(enhancer.repair({...testItem3}).durability).toStrictEqual(100);
     })
 });
 
 describe('enhancer - sucess', () => {
 
-    it ('returns enhanced item', () => {
-        const testItem = {
-            name: 'test sword',
-            enhancement: 0,
-            durability:0,
-        }
-        const expectedItem = {
-            name: 'test sword',
-            enhancement: 1,
-            durability:0,
-        } ;       
-        expect(enhancer.success(testItem)).toStrictEqual(expectedItem);
+    it('returns enhanced item', () => {
+
+        expect(enhancer.success({...testItem1}).enhancement).toStrictEqual(testItem1.enhancement + 1);
+        expect(enhancer.success({...testItem2}).enhancement).toStrictEqual(testItem2.enhancement + 1);
+        expect(enhancer.success({...testItem3}).enhancement).not.toStrictEqual(testItem3.enhancement + 1);
         
     })
 
-    it ('doesnt enhance item over 20', () => {
-        const testItem = {
-            name: 'test sword',
-            enhancement: 20,
-            durability:0,
-        };
-        const expectedItem = {
-            name: 'test sword',
-            enhancement: 20,
-            durability:0,
-        };       
-        expect(enhancer.success(testItem)).toStrictEqual(expectedItem);
+    it ('doesnt enhance item over 20', () => {       
+        expect(enhancer.success({...testItem3}).enhancement).toStrictEqual(testItem3.enhancement);
     });
 
     it ('does not change durability', () => {
-        const testItem = {
-            name: 'test sword',
-            enhancement: 10,
-            durability:10,
-        };
-        const expectedItem = {
-            name: 'test sword',
-            enhancement: 11,
-            durability:10,
-        };
-        expect(enhancer.success(testItem)).toStrictEqual(expectedItem);
-    })
+        expect(enhancer.success({...testItem1}).durability).toStrictEqual(testItem1.durability);
+        expect(enhancer.success({...testItem2}).durability).toBe(testItem2.durability);
+        expect(enhancer.success({...testItem3}).durability).toStrictEqual(testItem3.durability)})
 });
 
 describe('enhance - fail', () => {
+
     it('decreases item durability by 5 when enhancement is <15', () => {
-        const testItem = {
-            name: 'lucky amulet',
-            enhancement: 13,
-            durability: 6,
-        }
-        expect(enhancer.fail({...testItem})).toStrictEqual({
-            ...testItem,
-            durability: testItem.durability - 5
-        });
+        
+        expect(enhancer.fail({...testItem1}).durability).not.toBe(testItem1.durability - 5);
+        expect(enhancer.fail({...testItem2}).durability).not.toBe(testItem2.durability - 5);
+        expect(enhancer.fail({...testItem3}).durability).not.toBe(testItem3.durability - 5);
+    })
+
+    it('decreases item durability by 10 when enhancement is >= 15', () => {
+
+        expect(enhancer.fail({...testItem1}).durability).not.toBe(testItem1.durability - 10);
+        expect(enhancer.fail({...testItem2}).durability).toBe(testItem2.durability - 10);
+        expect(enhancer.fail({...testItem3}).durability).not.toBe(testItem3.durability - 10);    });
+
+    it('decreases item enhancement by 1 when enhancement is > 16', () => {
+
+        //console.log(enhancer.fail({...testItem}));
+        expect(enhancer.fail({...testItem1}).enhancement).not.toBe(testItem1.enhancement - 1);
+        expect(enhancer.fail({...testItem2}).enhancement).toBe(testItem2.enhancement - 1);
+        expect(enhancer.fail({...testItem3}).enhancement).toBe(testItem3.enhancement - 1);
     })
 })
